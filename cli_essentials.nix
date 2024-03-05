826cad292887b827ebe20bf7b91c7ca56b015a4b
FPS <mista.tapas@gmx.net>
2024-03-05 14:28:48 +0100

    add missing file

---
+++ cli_essentials.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{ 
+  options.cli_essentials = { 
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
+  };
+
+  config = {
+    nixpkgs.config.allowUnfree = true;
+    nixpkgs.overlays = [ ];
+
+    environment.systemPackages = with pkgs; 
+      (lib.mkIf config.cli_essentials.enable [
+        git htop iotop killall psmisc vim man-pages man-pages-posix bash-completion tmux zerovpn
+      ]);
+  };
+} 
