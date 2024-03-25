94d8b0fd9cf94eb929f9698938c4dad0999c587f
Florian Paul Schmidt <mista.tapas@gmx.net>
2024-03-25 13:00:16 +0100

    yay

---
+++ pkgs/sound/fixed-ir-Kalthallen-lv2.nix
@@ -0,0 +1,21 @@
+{ lib, stdenv, pkgs }:
+
+stdenv.mkDerivation rec {
+  pname = "fixed-ir-lv2";
+  version = "1";
+
+  src = ./fixed-ir-Kalthallen.lv2;
+
+  irs = builtins.fetchurl "https://shop.kalthallen.de/wp-content/uploads/2022/05/KalthallenCabsIR.rar";
+
+  doCheck = false;
+
+  buildInputs =  with pkgs; [ gnumake lv2 lilv libsamplerate shellcheck libsndfile fixed-ir-lv2 unrar ];
+
+  meta = with lib; {
+    platforms = platforms.all;
+  };
+
+  PREFIX = placeholder "out";
+}
+
